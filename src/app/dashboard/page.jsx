f26bb82168d40ebf09bec4f8556b64fdc83cea59
Fahry Firdaus <ffahry169@gmail.com>
2026-02-13 09:07:53 +0700

    feat: remove skeleton

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -183,7 +183,6 @@ const DashboardContent = () => {
             </div>
             <div className="flex gap-2">
               <Skeleton className="h-10 w-32 rounded-lg" />
-              <Skeleton className="h-10 w-32 rounded-lg" />
               <Skeleton className="h-10 w-28 rounded-lg" />
             </div>
           </div>
